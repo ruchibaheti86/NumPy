4dc4ec2e5d61b44e8c7036a3dfe306de0f81e0ed
ruchibaheti86 <61480735+ruchibaheti86@users.noreply.github.com>
2020-06-27 18:52:56 -0400

    NumPy Sorting

---
+++ SortNumPy.py
@@ -0,0 +1,22 @@
+import numpy as np
+#Sorting the numeric array
+arr = np.array([8,4,2,9,15])
+print("Array: ",arr)
+print("Sorting in Array: ")
+print(np.sort(arr))
+#Sorting the strings:
+arr = np.array(['Ram', 'Adam', 'Cherry'])
+print("Array: ",arr)
+print("Sorting in Array: ")
+print(np.sort(arr))
+#Sorting the boolean array
+arr = np.array([True, False, True, False])
+print("Array: ",arr)
+print("Sorting of array: ")
+print(np.sort(arr))
+#Sorting the 2D array
+#arr = np.array([[3,2,4], [5,0,1]])
+arr = np.array([[8,4,2,9,15], [13,23,45,65,24]])
+print("Array: ",arr)
+print("Sorting in 2D Array: ")
+print(np.sort(arr))
